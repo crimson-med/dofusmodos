cc72802cef0027b0f5441d34eb8faac6f94159c1
Raphael Mortier <raphaelmortier@outlook.Fr>
2019-01-15 10:35:23 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
 import React, { FunctionComponent } from "react";
 import "./App.css";
+import { Form, Image, Button, Label, Grid, Input } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
+import ReCAPTCHA from "react-google-recaptcha";
 import Modo from "./components/modo";
 
 const App: FunctionComponent = props => {
   return (
     <div className="App">
-      <Modo />
+      <Form size="tiny">
+        <Grid centered columns={2}>
+          <Grid.Column>
+            <Image centered src="./img/modo.png" size="small" />
+            <Form.Field>
+              <label>Nom du modérateur</label>
+              <Input required placeholder="Tiavos..." />
+            </Form.Field>
+            <Form.Field>
+              <label>Serveur</label>
+              <Input placeholder="Ush/Oshimo..." />
+            </Form.Field>
+            <Form.Field>
+              <label>Dernière position</label>
+              <Input placeholder="Bonta/Brakmar..." />
+            </Form.Field>
+            <Form.Field>
+              <label>Action</label>
+              <Input placeholder="Ban/Mute..." />
+            </Form.Field>
+            <ReCAPTCHA sitekey="6Le24YkUAAAAAPDiehfRMWwNeBhg-WFg3ii0Xltr" />
+            <Button type="submit">Partager</Button>
+          </Grid.Column>
+        </Grid>
+      </Form>
     </div>
   );
 };
