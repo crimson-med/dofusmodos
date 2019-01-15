348b17f5f4b5073022b6d4bad7d8e48eb4b81a43
Raphael Mortier <raphaelmortier@outlook.Fr>
2019-01-15 18:47:36 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,135 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState, useRef } from "react";
 import "./App.css";
-import { Form, Image, Button, Grid, Input } from "semantic-ui-react";
+import {
+  Form,
+  Image,
+  Button,
+  Grid,
+  Input,
+  InputOnChangeData
+} from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
 import ReCAPTCHA from "react-google-recaptcha";
 import Modo from "./components/modo";
-import ApolloClient, { gql } from "apollo-boost";
+import { Mutation } from "react-apollo";
+import { gql } from "apollo-boost";
 
-const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql"
-});
-
-function validForm(): string {
-  const schema = gql`
-    {
-      mutation {
-        addModo(
-          modo: {
-            nom: "Valentin"
-            server: "Nidas"
-            evt: "Ban"
-            lastpos: "Brak"
-          }
-        ) {
-          nom
-          server
-          evt
-          lastpos
-        }
-      }
+const addModoGql = gql`
+  mutation AddModo($type: ModoInput!) {
+    addModo(modo: $type) {
+      nom
+      server
+      evt
+      lastpos
+      heure
     }
-  `;
-
-  return "";
-}
+  }
+`;
 
 const App: FunctionComponent = props => {
+  const [nom, setNom] = useState("");
+  const [serv, setServ] = useState("");
+  const [action, setAction] = useState("");
+  const [pos, setPos] = useState("");
+  const recaptchaRef = useRef<ReCAPTCHA>(null);
+
+  const nomChange = (
+    evt: React.ChangeEvent<HTMLInputElement>,
+    data: InputOnChangeData
+  ) => {
+    setNom(data.value);
+  };
+  const servChange = (
+    evt: React.ChangeEvent<HTMLInputElement>,
+    data: InputOnChangeData
+  ) => {
+    setServ(data.value);
+  };
+  const actionChange = (
+    evt: React.ChangeEvent<HTMLInputElement>,
+    data: InputOnChangeData
+  ) => {
+    setAction(data.value);
+  };
+  const posChange = (
+    evt: React.ChangeEvent<HTMLInputElement>,
+    data: InputOnChangeData
+  ) => {
+    setPos(data.value);
+  };
   return (
-    <div className="App">
-      <Form action={validForm()} size="tiny">
-        <Image centered src="img/modo.png" size="small" />
-        <Grid centered columns={1}>
-          <Grid.Column floated="left" width={6}>
-            <Form.Input
-              label="Nom du modérateur"
-              placeholder="Tiavos.. "
-              required
-            />
-            <Form.Input label="Serveur" placeholder="Ush/Oshimo.. " required />
-            <Form.Input label="Action" placeholder="Ban/Mute.. " required />
-            <Form.Input
-              label="Nom du modérateur"
-              placeholder="Tiavos.. "
-              required
-            />
-            <Grid>
-              <Grid.Row>
-                <Grid.Column width={4}>
-                  <Button type="submit">Partager</Button>
-                </Grid.Column>
-                <Grid.Column width={8}>
-                  <ReCAPTCHA sitekey="6Ldp44kUAAAAAGmacWivKV8_xX0GJaH0RkzOy4QW" />
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
-          </Grid.Column>
-          <Grid.Column floated="right" width={10}>
-            <Modo />
-          </Grid.Column>
-        </Grid>
-      </Form>
-    </div>
+    <Mutation mutation={addModoGql}>
+      {(addModo, { data }) => (
+        <Form
+          onSubmit={e => {
+            e.preventDefault();
+            if (
+              !recaptchaRef ||
+              (recaptchaRef && recaptchaRef.current != null)
+            ) {
+              return;
+            }
+            const recaptchaValue = recaptchaRef.current!.getValue();
+            if (!recaptchaValue) {
+              return;
+            }
+            addModo({
+              variables: {
+                type: { nom: nom, server: serv, lastpos: pos, evt: action }
+              }
+            });
+          }}
+          size="tiny"
+        >
+          <Image centered src="img/modo.png" size="small" />
+
+          <Grid centered columns={1}>
+            <Grid.Column floated="left" width={6}>
+              <Form.Input
+                onChange={nomChange}
+                label="Nom du modérateur"
+                placeholder="Tiavos.. "
+                required
+              />
+              <Form.Input
+                onChange={servChange}
+                label="Serveur"
+                placeholder="Ush/Oshimo.. "
+                required
+              />
+              <Form.Input
+                onChange={actionChange}
+                label="Action"
+                placeholder="Ban/Mute.. "
+                required
+              />
+              <Form.Input
+                onChange={posChange}
+                label="Dernière position"
+                placeholder="Bonta/Brak.. "
+                required
+              />
+              <Grid>
+                <Grid.Row>
+                  <Grid.Column width={4}>
+                    <Button type="submit">Partager</Button>
+                  </Grid.Column>
+                  <Grid.Column width={8}>
+                    <ReCAPTCHA
+                      ref={recaptchaRef}
+                      sitekey="6Ldp44kUAAAAAGmacWivKV8_xX0GJaH0RkzOy4QW"
+                    />
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+            </Grid.Column>
+            <Grid.Column floated="right" width={10}>
+              <Modo />
+            </Grid.Column>
+          </Grid>
+        </Form>
+      )}
+    </Mutation>
   );
 };
 
