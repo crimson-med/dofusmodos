a6cf87e55691783333f42bae9c250f819fb7a044
unknown <raphaelmortier@outlook.Fr>
2019-01-20 23:07:39 +0100

    wip

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import ApolloClient, { gql } from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql"
+  uri: process.env.LINK_MODO
 });
 
 ReactDOM.render(
